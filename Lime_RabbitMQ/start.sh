a9d4f8e44b15adaa7c56d4afd4c6d7122f0e53b8
Samuel Wilk <34423885+da-wilky@users.noreply.github.com>
2023-11-06 18:56:21 +0100

    Update start.sh

--- Lime_RabbitMQ/start.sh
+++ Lime_RabbitMQ/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 composer install
 mkdir /output
-zip -x .gitignore -x start.sh -x composer.* -r /output/Lime_RabbitMQ.zip .
+zip -x .gitignore -x start.sh -x composer.* -x todo.md -r /output/Lime_RabbitMQ.zip .
