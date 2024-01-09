6c00d4238be36bdabe77c1a840e82dc3d615cdf3
Samuel Wilk <34423885+da-wilky@users.noreply.github.com>
2024-01-09 02:54:28 +0100

    Added Update button (triggers update signal on button click)

--- Lime_RabbitMQ/Lime_RabbitMQ.php
+++ Lime_RabbitMQ/Lime_RabbitMQ.php
@@ -83,6 +83,49 @@ class Lime_RabbitMQ extends PluginBase
         $this->subscribe('newSurveySettings');
         $this->subscribe('afterSurveyComplete');
         $this->subscribe('beforeSurveySettings');
+
+        $this->subscribe('newDirectRequest');
+    }
+
+    /**
+     * @return void
+     */
+    public function newDirectRequest()
+    {
+        if (!$this->getEvent()) {
+            throw new CHttpException(403);
+        }
+        if ($this->getEvent()->get('target') != get_class($this)) {
+            return;
+        }
+        if (!Permission::model()->hasGlobalPermission('superadmin')) {
+            throw new CHttpException(403);
+        }
+        $surveyId = App()->getRequest()->getQuery('sid', App()->getRequest()->getQuery('surveyId'));
+        if ($surveyId && App()->getRequest()->getQuery('method') == 'updateTable') {
+            $this->updateTable($surveyId);
+            App()->end(); // Not needed but more clear
+        }
+    }
+
+    public function updateTable($surveyId)
+    {
+        // SEND DATA
+        try {
+            $exchange = 'router';
+            $queue = 'msgs';
+            $connection = new AMQPStreamConnection($this->get('mq_host'), $this->get('mq_port'), $this->get('mq_user'), $this->get('mq_password'));
+            $channel = $connection->channel();
+            $channel->queue_declare($queue, false, true, false, false);
+            $channel->exchange_declare($exchange, AMQPExchangeType::DIRECT, false, true, false);
+            $channel->queue_bind($queue, $exchange);
+            $data = new stdClass();
+            $data->id = $surveyId;
+            $message = new AMQPMessage(json_encode($data), array('content_type' => 'application/json', 'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT));
+            $channel->basic_publish($message, $exchange);
+        } catch (Exception $e) {
+            $this->log($e);
+        }
     }
 
     /**
@@ -101,22 +144,7 @@ class Lime_RabbitMQ extends PluginBase
             return;
         }
 
-        // SEND DATA
-        try {
-            $exchange = 'router';
-            $queue = 'msgs';
-            $connection = new AMQPStreamConnection($this->get('mq_host'), $this->get('mq_port'), $this->get('mq_user'), $this->get('mq_password'));
-            $channel = $connection->channel();
-            $channel->queue_declare($queue, false, true, false, false);
-            $channel->exchange_declare($exchange, AMQPExchangeType::DIRECT, false, true, false);
-            $channel->queue_bind($queue, $exchange);
-            $data = new stdClass();
-            $data->id = $this->getEvent()->get('surveyId');
-            $message = new AMQPMessage(json_encode($data), array('content_type' => 'application/json', 'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT));
-            $channel->basic_publish($message, $exchange);
-        } catch (Exception $e) {
-            $this->log($e);
-        }
+        $this->updateTable($this->getEvent()->get('surveyId'));
     }
 
     /**
@@ -125,6 +153,14 @@ class Lime_RabbitMQ extends PluginBase
     public function beforeSurveySettings()
     {
         $event = $this->getEvent();
+        $url = Yii::app()->createUrl(
+            'plugins/direct',
+            array(
+                'plugin' => get_class($this),
+                'method' => 'updateTable',
+                'surveyId' => $event->get('survey')
+            )
+        );
         $event->set(
             "surveysettings.{$this->id}",
             [
@@ -145,6 +181,13 @@ class Lime_RabbitMQ extends PluginBase
                         ),
                         'help' => gT('Send the SurveyID to RabbitMQ after completion.')
                     ],
+                    'send_now' => [
+                        'type' => 'link',
+                        'label' => gT('Send Update'),
+                        'link' => $url,
+                    ],
+
+
                     // 'qustions' => [
                     //     'type' => 'list',
                     //     'label' => gT('Fragen'),
@@ -153,6 +196,7 @@ class Lime_RabbitMQ extends PluginBase
                 ]
             ]
         );
+
         // print_r($event->get('survey') . '<br>');
         // foreach ($this->api->getQuestions($event->get('survey'), 'de-informal') as $q) {
         //     print_r($q->title . ' ' . $q->preg . ' ' . $q->GetBasicFieldName() . '<br>');
